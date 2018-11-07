114c2296eea7b84441cbc78eca655a37847879b5
munhyunsu <munhyunsu@gmail.com>
2018-11-07 14:00:12 +0900

    working

--- mapping_word.py
+++ mapping_word.py
@@ -1,50 +1,53 @@
-# coding=utf-8
-import hgtk
+import csv
 import decompose
-import Dic
+
+import hgtk
+
+import dic 
 
 
-class MappingWord:
+DATAFILE = 'offensive_word_db.csv'
+
+
+class MappingWord(object):
     def __init__(self):
-        pass
+        self.word_list = self.read_word()
 
     def read_word(self):
-        split_result = list()
-        all_list = list()
-        with open('slang1.csv') as slang_file:
-            while True:
-                word= slang_file.readline()
-                if word is '\n':
-                    break
-                result = self.preprocessing(word)
-                all_list.append(result)
-        return all_list
-    def preprocessing(self,word):
+        word_list = list()
+        with open(DATAFILE) as f:
+            reader = csv.reader(f)
+            for row in reader:
+                word_list.append(row[0])
+        return word_list
+
+    def preprocessing(self, word):
+        replace_char = ['\n', ' ', 'ᴥ']
+
         result = hgtk.text.decompose(word)
 
+        for char in replace_char:
+            result = result.replace(char, '')
 
-        result = result.replace("\n","")
-        result = result.replace(" ","")
-        result = result.replace('ᴥ','')
         return result
-    def multi_eumso_to_single_eumso(self,word):
+
+    def multi_eumso_to_single_eumso(self, word):
         single_eumso = list()
-        multi_eumso = Dic.multi_eumso
+        multi_eumso = dic.multi_eumso
+
         for i in range(len(word)):
             if word[i] in multi_eumso:
                 changing_word = multi_eumso[word[i]]
-                single = self.add_eumso(single_eumso,changing_word)
-                #single_eumso +=single
+                single = self.add_eumso(single_eumso, changing_word)
             else:
                 single_eumso.append(word[i])
         return single_eumso
 
-    def add_eumso(self,single_eumso,changing_word):
+    def add_eumso(self, single_eumso, changing_word):
         for i in range(len(changing_word)):
             single_eumso.append(changing_word[i])
         return single_eumso
 
-    # 보류
     def num_to_hangeul(self,num):
         divide_num = num
         korean = list()
@@ -59,9 +62,6 @@ class MappingWord:
             how_many_divide +=1
         return korean
 
-
-
-
     def mapping_number(self):
         word_list = self.read_word()
         all_list = list()
@@ -70,6 +70,11 @@ class MappingWord:
             all_list.append(word)
         return all_list[:-2]
 
-if  __name__ == '__main__':
+
+def main():
     map = MappingWord()
     map.mapping_number()
+
+
+if __name__ == '__main__':
+    main()
